270c31bf2525003842d4cb6fe92ddb35e7dff474
drmanio <drmanio@gmail.com>
2021-10-19 11:02:03 +0200

    Inizializzato il file index.php

--- index.php
+++ index.php
@@ -14,13 +14,11 @@
                 });
             });
         </script>
-
     </head>
     <body>
-      <p>Questa è una prova</p>
         <ul class="nav nav-pills nav-fill">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">File xml</a>
+              <a class="nav-link" aria-current="page" href="#">File xml</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Much longer nav link</a>
