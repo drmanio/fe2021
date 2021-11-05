11a7043d56f5b857916458cc197119fdecfae121
drmanio <drmanio@gmail.com>
2021-11-05 15:05:03 +0100

    corretto errore scadenza pagamento nulla

--- save_scadenze.php
+++ save_scadenze.php
@@ -5,7 +5,7 @@ $info=array($_POST['info1'],$_POST['info2'],$_POST['info3'],$_POST['info4'],$_PO
 $info=str_replace ("'","\'",$info);
 $istruzione_sql= "INSERT INTO scadenzario(idFE, modPagamento, scadenzaPagamento, importoPagamento, IBAN, Pagato, 
 DataPagamento, ModoPagamento, Note) 
-VALUES('$info[0]','$info[1]','$info[2]','$info[3]','$info[4]','$info[5]',NULLIF('$info[6]',''),'$info[7]','$info[8]')";
+VALUES('$info[0]','$info[1]',NULLIF('$info[2]',''),'$info[3]','$info[4]','$info[5]',NULLIF('$info[6]',''),'$info[7]','$info[8]')";
 $test = mysqli_query($connessioneDB, $istruzione_sql);
 if (!$test){
     //SETTAGGIO MESSAGGIO DI ERRORE
