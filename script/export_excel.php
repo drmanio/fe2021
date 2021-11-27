fbccda45687ca2c8c89ed935328d257847f29997
drmanio <drmanio@gmail.com>
2021-11-27 07:32:18 +0100

    modifica esportazione in excel

--- script/export_excel.php
+++ script/export_excel.php
@@ -8,9 +8,11 @@
 		 
 		 echo "<table>";
 				echo "<thead>";
-				echo "<th>id</th>";				
+				echo "<th>id</th>";
+        echo "<th>IdFornitore</th>";				
 				echo "<th>Fornitore</th>";
 				echo "<th>IBAN</th>";
+        echo "<th>nr_documento</th>";
 				echo "<th>Importo</th>";
 				echo "<th>Note</th>";
 				echo "<th>Data pagamento</th>";
@@ -18,16 +20,23 @@
 				echo "<tbody>";
 				
 			$query = "SELECT 
-			idScadenzario, forn_den, IBAN, importoPagato, Note, DataPagamento 
-			FROM pagamenti_temp WHERE idAzienda = '$idazienda'";
+			idScadenzario, forn_piva, forn_den, IBAN, doc_nr, importoPagato, Note, DataPagamento 
+			FROM pagamenti_temp WHERE idAzienda = '$idazienda' ORDER BY forn_den";
 			
 				$result = mysqli_query($connessioneDB,$query);
 				while($row = mysqli_fetch_array($result)){
 					//echo print_r($row);
+          if ($row['forn_piva']==""){
+            $forn_piva = "ND";
+          } else {
+            $forn_piva = $row['forn_piva']; 
+          }
 					echo "<tr>";
 					echo "<td>".$row['idScadenzario']."</td>";
+          echo "<td>".$forn_piva."</td>";
 					echo "<td>".$row['forn_den']."</td>";
-					echo "<td>".$row['IBAN']."</td>";					
+					echo "<td>".$row['IBAN']."</td>";
+          echo "<td>".$row['doc_nr']."</td>";					
 					echo "<td>".str_replace(".",",",$row['importoPagato'])."</td>";
 					echo "<td>".$row['Note']."</td>";
 					echo "<td>".$row['DataPagamento']."</td>";
