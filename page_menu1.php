19a8bd127b2715ce39c4377ea75ba83f30217c26
drmanio <drmanio@gmail.com>
2021-12-02 00:51:42 +0100

    analisi e miglioramento del page menu1

--- page_menu1.php
+++ page_menu1.php
@@ -6,6 +6,7 @@
       include "form.php";
       include "fe_nodi.php";
       include "function.php";
+      include "script".DIRECTORY_SEPARATOR."upload.php";
     ?>
 
     
@@ -26,15 +27,6 @@ include "navbar.html";
 
 <!--
 FORM INIZIALE PER SELEZIONARE IL FILE E CHIEDERNE LA VISUALIZZAZIONE 
-The enctype attribute specifies how the form-data should be encoded when submitting it to the server.
-Note: The enctype attribute can be used only if method="post".
-Attribute Values:
-- application/x-www-form-urlencoded ** Default. All characters are encoded before sent (spaces are converted to "+" symbols, and special characters are converted to ASCII HEX values)
-- multipart/form-data ** This value is necessary if the user will upload a file through the form
-- text/plain ** Sends data without any encoding at all. Not recommended 
-
-$ _SERVER [ 'PHP_SELF'] è una variabile d'ambiente supportata da tutte le piattaforme che indica il nome del file su cui è attualmente in esecuzione lo script PHP rispetto alla root del Web server.
-$ _SERVER [ 'PHP_SELF'] è comodo perché rende il codice di un form riutilizzabile, non dovrai infatti cambiare ogni volta l'argomento riferito all'ACTION.
 -->
 
 <!-- With PHP, it is easy to upload files to the server. -->
@@ -53,23 +45,36 @@ $ _SERVER [ 'PHP_SELF'] è comodo perché rende il codice di un form riutilizzab
 <!-- The type="file" attribute of the <input> tag shows the input field as a file-select control, with a "Browse" button next to the input control -->
 <!-- The form sends data to a file called "upload.php" -->
 
-<!-- Because in different OS there is different directory separator. In Windows it's \ in Linux it's /. DIRECTORY_SEPARATOR is constant with that OS directory separator. -->
-<form action = <?php echo '"script'.DIRECTORY_SEPARATOR.'upload.php"'; ?> method="post" enctype="multipart/form-data">
+<!-- $ _SERVER [ 'PHP_SELF'] è una variabile d'ambiente supportata da tutte le piattaforme che indica il nome del file su cui è attualmente in esecuzione lo script PHP rispetto alla root del Web server.
+$ _SERVER [ 'PHP_SELF'] è comodo perché rende il codice di un form riutilizzabile, non dovrai infatti cambiare ogni volta l'argomento riferito all'ACTION. -->
+<form action = <?php echo $_SERVER['PHP_SELF']; ?> method="post" enctype="multipart/form-data">
   <h3> Seleziona il file xml/p7m proveniente dallo Sdi da caricare:</h3>
   <input class="form-control" type="file" name="fileToUpload" id="fileToUpload">
   <br>
-  <input type="submit" id="btn_view_dati_xml" value="visualizza dati" name="submit" onclick="this.style.display='none'">
+  <input type="submit" id="btn_view_dati_xml" value="carica il file" name="submit" onclick="this.style.display='none'">
 </form>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-    <h3> Seleziona il file xml/p7m proveniente dallo Sdi da caricare:</h3>
-    <input class="form-control" type="file" name="file">
-    <br>
-    <input id="btn_view_dati_xml" type="submit" value="visualizza dati" name="submit" onclick="this.style.display='none'">
+<!--
+FORM INIZIALE PER SELEZIONARE IL FILE E CHIEDERNE LA VISUALIZZAZIONE 
+The enctype attribute specifies how the form-data should be encoded when submitting it to the server.
+Note: The enctype attribute can be used only if method="post".
+Attribute Values:
+- application/x-www-form-urlencoded ** Default. All characters are encoded before sent (spaces are converted to "+" symbols, and special characters are converted to ASCII HEX values)
+- multipart/form-data ** This value is necessary if the user will upload a file through the form
+- text/plain ** Sends data without any encoding at all. Not recommended 
+
+$ _SERVER [ 'PHP_SELF'] è una variabile d'ambiente supportata da tutte le piattaforme che indica il nome del file su cui è attualmente in esecuzione lo script PHP rispetto alla root del Web server.
+$ _SERVER [ 'PHP_SELF'] è comodo perché rende il codice di un form riutilizzabile, non dovrai infatti cambiare ogni volta l'argomento riferito all'ACTION.
+-->
+<!-- <form action="" method="post" enctype="multipart/form-data"> -->
+    <!-- <h3> Seleziona il file xml/p7m proveniente dallo Sdi da caricare:</h3> -->
+    <!-- <input class="form-control" type="file" name="file"> -->
+    <!-- <br> -->
+    <!-- <input id="btn_view_dati_xml" type="submit" value="visualizza dati" name="submit" onclick="this.style.display='none'"> -->
     <!-- <br><br>
     <button id="btn_new_file">Carica un nuovo file</button> -->
-</form>
-</div>
+<!-- </form> -->
+
 <div>
 <br>
 <!-- CODICE PHP CHE SI ATTIVA QUANDO VIENE PREMUTO IL PULSANTE CON NAME submit IN AGGIUNTA ALLA ACTION RIPORTATA NEL FORM -->
@@ -78,59 +83,63 @@ $ _SERVER [ 'PHP_SELF'] è comodo perché rende il codice di un form riutilizzab
 if (isset($_POST['submit'])){
     //ESEGUO LA FUNZIONE carica_xml() CHE ARCHIVIA IL FILE IN UNA CARTELLA DEL SERVER E RITORNA UN FILE IN FORMATO SIMPLEXML CHE ASSEGNO ALLA VARIABILE $xml_file                
     $xml_file = carica_xml();
-    //LA VARIABILE SUPERGLOBALE $_FILES E' UN ARRAY ASSOCIATIVO (chiavi: file) DI ARRAY ASSOCIATIVO (chiavi: name, type, tmp_name, error, size).
-    //PER RECUPERARE IL NOME DEVO RECUPERARE IL VALORE DI name DELLA CHIAVE file
-    $fileName = basename($_FILES['file']['name']);
-    // echo $fileName;
-    // echo "<br>";
-    $ext = pathinfo($fileName, PATHINFO_EXTENSION);
-    // echo $ext;
-    // echo "<br>";
-    $fileout = pathinfo($fileName, PATHINFO_FILENAME);
-    // echo $fileout;
-    // echo "<br>";
-    if ($ext=="p7m"){
-        $_SESSION['nomeFile'] = $fileout;
-    } else {
-        $_SESSION['nomeFile'] = $fileName;
+
+    if ($xml_file){
+      //LA VARIABILE SUPERGLOBALE $_FILES E' UN ARRAY ASSOCIATIVO (chiavi: file) DI ARRAY ASSOCIATIVO (chiavi: name, type, tmp_name, error, size).
+      //PER RECUPERARE IL NOME DEVO RECUPERARE IL VALORE DI name DELLA CHIAVE file
+      $fileName = basename($_FILES['fileToUpload']['name']);
+      // echo $fileName;
+      // echo "<br>";
+      $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+      // echo $ext;
+      // echo "<br>";
+      $fileout = pathinfo($fileName, PATHINFO_FILENAME);
+      // echo $fileout;
+      // echo "<br>";
+      if ($ext=="p7m"){
+          $_SESSION['nomeFile'] = $fileout;
+      } else {
+          $_SESSION['nomeFile'] = $fileName;
+      }
+      // echo "Nome del file caricato: ".$fileName."<br/>";
+      // echo "<br>";
+      // VERIFICO SE IL FILE SIMPLEXML E' STATO ATTRIBUITO ALLA VARIABILE
+      // if($xml_file){ 
+      //     echo ("Tutto ok, file caricato");
+      //     echo "<br/>";
+      // }
+      // VISUALIZZO I DATI DELLA FATTURA ELETTRONICA CONTENUTI NEL FILE SIMPLEXML ATTRAVERSO LA FUNZIONE visualizza_dati_xml PRESENTE NEL FILE function.php
+      echo "<br><b>VISUALIZZA I DATI CONTENUTI NEL FILE XML </b>";
+      echo "<button id='btn_table_xml' class='btn btn-primary' type='button'>Dati file xml</button>";
+      // RICHIAMO LA FUNZIONE PER VISUALIZZARE I DATI IN FORMA DI TABELLA
+      visualizza_dati_xml($xml_file);
+      // $_SESSION['file'] = $xml_file;
+      // RICHIAMO LA FUNZIONE crea_id() CONTENUTA NEL FILE function.php PER ASSEGNARE ALLA VARIABILE $id_db L'ID UNIVOCO DELLA FATTURA XML
+      // CHE VERRA' MEMORIZZATO NEL DATABASE
+      $id_db=crea_id();
+      //*********************************
+      // MEMORIZZO L'ID UNIVOCO CONTENUTO NELLA VARIABILE $id_db IN UNA VARIABILE SUPER GLOBALE DI SESSIONE id
+      $_SESSION['id'] = $id_db;
+      //*********************************
+      // SCRIVO A VIDEO L'ID GENERATO
+      //echo "ID univoco database: ".$id_db;
+      //echo "<br/>";
+      //RECUPERO I DATI GENERALI DELLA FATTURA E LI INSERISCO IN UN FORM PER EVENTUALI MODIFICHE E PER PROCEDERE POI CON LA MEMORIZZAZIONE                
+      //I DATI GENERALI SONO CONTENUTI NELL'ARRAY $dati_generali CONTENUTO NEL FILE nodi_fe.php CHE E' STATO INCLUSO
+      //RICHIAMO LA FUNZIONE form_dati_generali() CHE VISUALIZZA E PERMETTE DI MEMORIZZARE I DATI
+      form_dati_generali($dati_generali, $xml_file);
+      // $_SESSION['scadenze'] = $scadenze_db;
+      echo "<br/>";
+      //RECUPERO I DATI SUI PAGAMENTI E LI INSERISCO IN UN FORM PER EVENTUALI MODIFICHE E PER PROCEDERE POI CON LA MEMORIZZAZIONE                
+      //I DATI GENERALI SONO CONTENUTI NELL'ARRAY $scadenze CONTENUTO NEL FILE nodi_fe.php CHE E' STATO INCLUSO
+      //RICHIAMO LA FUNZIONE form_scadenze() CHE VISUALIZZA E PERMETTE DI MEMORIZZARE I DATI
+      form_scadenze($scadenze, $xml_file);
+      echo "<br/>";
+      form_ritenute($ritenute, $xml_file);
+      echo "<br/>";
+      form_beniservizi($beniservizi, $ddt, $xml_file);
     }
-    // echo "Nome del file caricato: ".$fileName."<br/>";
-    // echo "<br>";
-    // VERIFICO SE IL FILE SIMPLEXML E' STATO ATTRIBUITO ALLA VARIABILE
-    // if($xml_file){ 
-    //     echo ("Tutto ok, file caricato");
-    //     echo "<br/>";
-    // }
-    // VISUALIZZO I DATI DELLA FATTURA ELETTRONICA CONTENUTI NEL FILE SIMPLEXML ATTRAVERSO LA FUNZIONE visualizza_dati_xml PRESENTE NEL FILE function.php
-    echo "<br><b>VISUALIZZA I DATI CONTENUTI NEL FILE XML </b>";
-    echo "<button id='btn_table_xml' class='btn btn-primary' type='button'>Dati file xml</button>";
-    // RICHIAMO LA FUNZIONE PER VISUALIZZARE I DATI IN FORMA DI TABELLA
-    visualizza_dati_xml($xml_file);
-    // $_SESSION['file'] = $xml_file;
-    // RICHIAMO LA FUNZIONE crea_id() CONTENUTA NEL FILE function.php PER ASSEGNARE ALLA VARIABILE $id_db L'ID UNIVOCO DELLA FATTURA XML
-    // CHE VERRA' MEMORIZZATO NEL DATABASE
-    $id_db=crea_id();
-    //*********************************
-    // MEMORIZZO L'ID UNIVOCO CONTENUTO NELLA VARIABILE $id_db IN UNA VARIABILE SUPER GLOBALE DI SESSIONE id
-    $_SESSION['id'] = $id_db;
-    //*********************************
-    // SCRIVO A VIDEO L'ID GENERATO
-    //echo "ID univoco database: ".$id_db;
-    //echo "<br/>";
-    //RECUPERO I DATI GENERALI DELLA FATTURA E LI INSERISCO IN UN FORM PER EVENTUALI MODIFICHE E PER PROCEDERE POI CON LA MEMORIZZAZIONE                
-    //I DATI GENERALI SONO CONTENUTI NELL'ARRAY $dati_generali CONTENUTO NEL FILE nodi_fe.php CHE E' STATO INCLUSO
-    //RICHIAMO LA FUNZIONE form_dati_generali() CHE VISUALIZZA E PERMETTE DI MEMORIZZARE I DATI
-    form_dati_generali($dati_generali, $xml_file);
-    // $_SESSION['scadenze'] = $scadenze_db;
-    echo "<br/>";
-    //RECUPERO I DATI SUI PAGAMENTI E LI INSERISCO IN UN FORM PER EVENTUALI MODIFICHE E PER PROCEDERE POI CON LA MEMORIZZAZIONE                
-    //I DATI GENERALI SONO CONTENUTI NELL'ARRAY $scadenze CONTENUTO NEL FILE nodi_fe.php CHE E' STATO INCLUSO
-    //RICHIAMO LA FUNZIONE form_scadenze() CHE VISUALIZZA E PERMETTE DI MEMORIZZARE I DATI
-    form_scadenze($scadenze, $xml_file);
-    echo "<br/>";
-    form_ritenute($ritenute, $xml_file);
-    echo "<br/>";
-    form_beniservizi($beniservizi, $ddt, $xml_file);         
+             
 }
 
 ?>
--- script/upload.php
+++ script/upload.php
@@ -1,80 +1,75 @@
 <!-- The "upload.php" file contains the code for uploading a file: -->
-
 <?php
 
-// PHP script explained:
+function carica_xml() {
+  // PHP script explained:
 
-// $target_dir = "uploads/" - specifies the directory where the file is going to be placed
-// Because in different OS there is different directory separator. In Windows it's \ in Linux it's /. DIRECTORY_SEPARATOR is constant with that OS directory separator.
-$target_dir = "uploads".DIRECTORY_SEPARATOR;
-// $target_file specifies the path of the file to be uploaded
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-// $uploadOk=1 is not used yet (will be used later)
-$uploadOk = 1;
+  // $target_dir = "uploads/" - specifies the directory where the file is going to be placed
+  // Because in different OS there is different directory separator. In Windows it's \ in Linux it's /. DIRECTORY_SEPARATOR is constant with that OS directory separator.
+  $target_dir = "uploads".DIRECTORY_SEPARATOR;
+  // $target_file specifies the path of the file to be uploaded
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  // $uploadOk=1 is not used yet (will be used later)
+  $uploadOk = 1;
 
+  // Now we can add some restrictions.
 
+  // Allow certain file formats
+  // $FileType holds the file extension of the file (in lower case)
+  $FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// $imageFileType holds the file extension of the file (in lower case)
-// Next, check if the image file is an actual image or a fake image
-
-
-
-
-
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
+  // The code below only allows users to upload xml and p7m files. All other file types gives an error message before setting $uploadOk to 0:
+  if($FileType != "xml" && $FileType != "p7m") {
+    echo "Sorry, only xml & p7m files are allowed.";
     $uploadOk = 0;
   }
-}
+    
+  // Check if file already exists
 
-// Check if file already exists
-
-// Now we can add some restrictions.
-
-// First, we will check if the file already exists in the "uploads" folder. If it does, an error message is displayed, and $uploadOk is set to 0:
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-
-// The file input field in our HTML form above is named "fileToUpload".
-
-// Now, we want to check the size of the file. If the file is larger than 500KB, an error message is displayed, and $uploadOk is set to 0:
-
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-
-// The code below only allows users to upload JPG, JPEG, PNG, and GIF files. All other file types gives an error message before setting $uploadOk to 0:
-
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
+  // First, we will check if the file already exists in the "uploads" folder. If it does, an error message is displayed, and $uploadOk is set to 0:
+  if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
   }
+
+  // Check file size
+
+  // The file input field in our HTML form above is named "fileToUpload".
+
+  // Now, we want to check the size of the file. If the file is larger than 900KB, an error message is displayed, and $uploadOk is set to 0:
+
+  if ($_FILES["fileToUpload"]["size"] > 900000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+  }
+
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo "<br>";
+    echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+
+      //LA FUNZIONE strcasecmp PERMETTE DI FARE UNA VERIFICA DELLE STRINGHE CASE INSENSITIVE
+      if (strcasecmp($FileType, "p7m") == 0) {
+        $file = "..".DIRECTORY_SEPARATOR."uploads".DIRECTORY_SEPARATOR.$target_file;
+        $fileout = pathinfo($target_file, PATHINFO_FILENAME);
+        $out = shell_exec('cd openssl & openssl smime -decrypt -in '.$file.' -inform DER -verify -noverify -out "..'.DIRECTORY_SEPARATOR.'uploads'.DIRECTORY_SEPARATOR.$fileout.'"');
+        // $out = shell_exec('cd openssl & openssl smime -verify -inform DER -in '.$file.' -noverify -out "..\uploads\\'.$fileout.'"');
+        $xml = simplexml_load_file($target_dir.$fileout);
+      } else {
+          $xml = simplexml_load_file($target_file);
+      }
+      return $xml;
+
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }
+  }
+
 }
+
+
 ?>
