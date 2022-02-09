cd8ba5bbfe1933dae382fc87ae94f3bfa3ba3e3e
drmanio <drmanio@gmail.com>
2022-02-09 09:55:43 +0100

    update archivio pagamenti

--- script/archivia_pag.php
+++ script/archivia_pag.php
@@ -5,8 +5,19 @@
 			
 	$query = "INSERT INTO 
 	pagamenti_scadenze (idScadenzario, importoPagamento, DataPagamento, ModoPagamento, Note) 
-	SELECT idScadenzario, importoPagato, DataPagamento, modPagamento, Note 
-	FROM pagamenti_temp WHERE idAzienda = '$idazienda'";
+	SELECT 
+  pag_tmp.idScadenzario, 
+  pag_tmp.importoPagato, 
+  pag_tmp.DataPagamento, 
+  pag_tmp.modPagamento, 
+  pag_tmp.Note 
+	FROM 
+  pagamenti_temp pag_tmp 
+  INNER JOIN 
+  fe2021.bonifici_tmp bon_tmp
+  ON 
+  pag_tmp.idAzienda = '$idazienda' AND 
+  pag_tmp.idPagamentiTemp = bon_tmp.idPagamentiTemp";
 
 	$result = mysqli_query($connessioneDB,$query);
 
